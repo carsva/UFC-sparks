a0dbaa2001d1919c90b82c7bd5a9d7f1b7243f6f
Carl Svahnstrom <carl@tocco.se>
2018-08-20 23:28:42 +0200

    Managed to get hold of the fighter data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { AppConsumer } from '../AppContext';
+import axios from 'axios';
+
+
 
 class Home extends Component {
+
+  ufc() {
+   axios.get('http://ufc-data-api.ufc.com/api/v1/us/fighters')
+  .then(function (response) {
+    var fighters = response.data;
+    return(fighters);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  }
+
+  
+
   render() {
+    
     return (
       <AppConsumer>
         {context => (
@@ -11,7 +33,7 @@ class Home extends Component {
               <h1>Fighter1</h1> vs <h1>Fighter2</h1>
             </div>
             <div className="ordinary">
-              <button onClick={context.test}>Test</button>
+              <button onClick={this.ufc}>Test</button>
             </div>
           </div>
         )}
