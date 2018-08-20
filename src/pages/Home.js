e1a024305520cb6fe91f48031143c3bd6c2cb493
Carl Svahnstrom <carl@tocco.se>
2018-08-20 17:16:59 +0200

    First basic CSS layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { AppConsumer } from '../AppContext';
 
 class Home extends Component {
- 
   render() {
     return (
       <AppConsumer>
         {context => (
-          <div>Hello</div>
+          <div className="container">
+            <div className="fighter">
+              <h1>Fighter1</h1> vs <h1>Fighter2</h1>
+            </div>
+            <div className="ordinary">
+              <button onClick={context.test}>Test</button>
+            </div>
+          </div>
         )}
       </AppConsumer>
     );
